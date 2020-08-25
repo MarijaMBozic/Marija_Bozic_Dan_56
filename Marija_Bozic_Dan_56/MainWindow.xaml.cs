a98c0fbb8d9e5db9f620ae86fbd2f00d6a24b701
MarijaMBozic <60043521+MarijaMBozic@users.noreply.github.com>
2020-08-25 10:37:30 +0200

    adding saveHtml file

--- Marija_Bozic_Dan_56/MainWindow.xaml.cs
+++ Marija_Bozic_Dan_56/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Compression;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -24,6 +25,25 @@ namespace Marija_Bozic_Dan_56
         public MainWindow()
         {
             InitializeComponent();
-        }       
+        }
+
+        private void btnSaveHtml_Click(object sender, RoutedEventArgs e)
+        {
+            string[] inputAddress = txtWebAddress.Text.Split('.');
+            string fileName = inputAddress[1].ToString();
+
+            using (WebClient client = new WebClient())
+            {
+                client.DownloadFile(txtWebAddress.Text, string.Format(@"C:..\..\FilesHTML\{0}.html", fileName));
+            }
+        }
+
+        private void btnZipFile_Click(object sender, RoutedEventArgs e)
+        {
+            using (ZipArchive zip = ZipFile.Open("test.zip", ZipArchiveMode.Create))
+            {
+                zip.CreateEntryFromFile(@"C:..\..\FilesHTML\blic.html", @"C:..\..\ZipFile\blic.html");
+            }
+        }
     }
 }
